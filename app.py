1f78e08866ceae80030691f3fe647bc378b603bd
ahmed14-cell <ahmedito14@gmail.com>
2020-12-13 10:55:23 +0300

    create api for performing predictions

---
+++ app.py
@@ -0,0 +1,34 @@
+'''This is a script to create API for predictions using our machine learing
+    model.
+'''
+import pickle
+import numpy as np
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+model = pickle.load(open('models/model.pickle', 'rb'))
+
+
+@app.route('/')
+def home():
+    '''Function to render our front end'''
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    ''' For rendering results '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template(
+        'index.html', prediction_text='Production is estimated to be {} Metric\
+             Tonnes'.format(output)
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=6060)
